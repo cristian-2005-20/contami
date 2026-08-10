e1027d552474c7ecc23dc4b835efc4804dbd7a3b
cristian-2005-20 <cristian.muzio2000@gmail.com>
2026-08-10 11:32:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    I dati delle finanze NON passano di qui: restano in localStorage,
    sul dispositivo, e non vengono mai inviati da nessuna parte. */
 
-const VERSIONE = "libromastro-v11";
+const VERSIONE = "libromastro-v12";
 const FILE = [
   "./",
   "./index.html",
