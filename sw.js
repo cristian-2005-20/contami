8d2bd3584cc817db3ebbe9f979799aa573aa0efb
cristian-2005-20 <cristian.muzio2000@gmail.com>
2026-08-14 14:20:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    I dati delle finanze NON passano di qui: restano in localStorage,
    sul dispositivo, e non vengono mai inviati da nessuna parte. */
 
-const VERSIONE = "contami-v4";
+const VERSIONE = "contami-v5";
 const FILE = [
   "./",
   "./index.html",
